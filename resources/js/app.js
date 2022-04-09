7302ae2926b62a2e790e839cc9e959fc949ec0a1
sujantimalsina-np <sujan.timalsina@apexcollege.edu.np>
2022-04-09 18:52:19 +0545

    todo list

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+require('./bootstrap');
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios, axios);
+
+Vue.component('todo', require('./components/Todo.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
